99bd8b18a8d0a8a032c51bf3b585f79adc430991
riya shah <71328725+riyaashah29@users.noreply.github.com>
2023-07-21 10:16:52 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://riyaa:riyaa@cluster0.3oboonb.mongodb.net/nest-mongo'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb/localhost/nest-mongo'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
